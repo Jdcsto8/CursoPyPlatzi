8d391392094bf6fd124038da81d2cc4764239522
jdc39 <denny.16@hotmail.com>
2026-08-27 19:04:31 -0500

    primeras clases

---
+++ SINTAXIS-IDENTACION/comentarios.py
@@ -0,0 +1,17 @@
+# esta es una linea de codigo 
+
+print("hola tu ") # imprime hola mundo 
+
+# este 
+# es 
+# un 
+# comentario
+
+""" 
+usted quien es 
+como se llama 
+
+
+"""
+
+#print("ignoremos est linea ")
