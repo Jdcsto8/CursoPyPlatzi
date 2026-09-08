8aeec9deca6a22ac16ec11e140b97209b49b9e73
Jdcsto8 <johndennycastano@gmail.com>
2026-09-08 16:35:26 -0500

    cargo archivos clase 15 a la 19

---
+++ OPERADORES/match.py
@@ -0,0 +1,16 @@
+menu = 5
+
+match menu:
+    case 1:
+       print("hamburgesa")
+    case 2:
+           print("pizza")
+    case 3:
+           print("picadas")
+    case 4:
+            print("helados")
+    case 5:
+           print("salchipapas")
+    case _:
+           print("la opcion seleccionada no coincide con los menus disponibles")
+
---
+++ OPERADORES/operadoresAsigancion.py
@@ -0,0 +1,26 @@
+x = 10
+y = 10
+z = 10
+w = 10
+f = 10
+m = 2
+e = 2
+x += 2 # suma 
+y -= 2 # resta
+z *= 2 # producto
+w /= 2 #
+f //= 2 # division entera
+m %= 2 # modulo 
+e **= 2 # exponente 
+print("suma = ",x)
+print("resta = ",y)
+print("producto = ",z)
+print("division = ",w)
+print("division Entera = ",f)
+print("modulo = ",m)
+print("exponente = ",e)
+
+# nuevo operador WALRUS (MORSA) :=
+print(x := 5)
+print(x)
+print(l :="hola")
