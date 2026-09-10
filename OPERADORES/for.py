6b7add6790b44d30094309374cb0c31a2945c6e5
Jdcsto8 <johndennycastano@gmail.com>
2026-09-10 18:50:56 -0500

    clase 20 y 21

---
+++ OPERADORES/for.py
@@ -0,0 +1,55 @@
+palabra = "simon"
+
+for letra in palabra:
+    print(letra)
+
+numeros = [1,2,33,45,77,88,99,100,555,777,33]    
+
+"""
+for numero in numeros:
+    print(numero)
+
+
+
+for numero in numeros:
+    if numero == 1:
+        break
+    print(numero)
+
+
+
+for numero in numeros:
+    if numero == 1:
+        continue
+    print(numero)
+
+
+
+for i in range(10):
+    print(i)
+
+
+
+for i in range(3,7,2):
+    print(i)
+
+
+
+departamentos = ["risaralada","Colombia"]
+ciudades = ["Pereira","La Virginia","Dosquebradas","Balboa","La Celia"]
+
+for departamento in departamentos:
+    for ciudad in ciudades:
+        print(ciudades,departamentos)
+
+
+"""
+
+n = [1,2,3]
+m = ["a","b"]
+
+for numeros in n:
+    for letras in m:
+        pass
+        #print(numeros,letras)
+
---
+++ OPERADORES/while.py
@@ -0,0 +1,51 @@
+i = 0
+"""
+#while i <= 10:
+    #print(i) 
+    #i += 1 # imprime hasta 10; si no se coloca i += 1 se imprimira 1 infinitamente
+
+while i <= 10:
+    i += 1 # imprime hasta 11; si no se coloca i += 1 se imprimira 1 infinitamente
+    print(i)
+
+while i <= 10:
+    i += 1 # imprime hasta 11; si no se coloca i += 1 se imprimira 1 infinitamente
+    print(i)
+    if i == 5:
+        break # detine el bucle 
+
+
+while i <= 10:
+    print(i)
+    if i == 5:
+        break # detine el bucle 
+
+    i += 1 # imprime hasta el 5
+    
+
+while i <= 10:
+   
+    if i == 5:
+        break # detine el bucle 
+    print(i)    
+    i += 1 # imprime hasta el 4
+ 
+
+while i <= 10:
+   
+    if i == 5:
+        break # detine el bucle 
+    i += 1 # imprime hasta el     
+    print(i)    
+
+
+"""        
+
+while i <= 10:
+    i += 1 # imprime hasta el   
+    if i == 5:
+        continue # detine el bucle 
+      
+    print(i)    
+else:
+    print("dejo de ser menor o igual a 10")
