bec6b835a50aec59521c591b36a961ebc8efaba6
Jdcsto8 <johndennycastano@gmail.com>
2026-09-15 17:40:32 -0500

    finalizacion clase 25

--- tipos-datos/diccionario.py
+++ tipos-datos/diccionario.py
@@ -17,3 +17,70 @@ print(articulos.keys())
 print(articulos.values())
 
 
+# saber si un elemento existe en el diccionario 
+if 1 in articulos: # solo valida la clave 
+    print("articulo existente ")
+else:
+    print("articulo no existe ")    
+
+# modificaciones 
+articulos[4] = "Bananos"
+#print(articulos)
+
+# agrega articulos
+articulos[5] = "Sandia"
+#print(articulos)
+articulos.update({4:"Mamoncillo",6:"Kiwui",7:"Durazno"})
+#print(articulos)
+
+# eliminacion 
+#articulos.pop(2)
+#print(articulos)
+#articulos.popitem() # elimina el ultimo item del diccionario
+#print(articulos)
+#articulos.clear() # vacia el contenido del diccionario
+#print(articulos)
+
+# recorre con un bulce el diccionario 
+#for k in articulos:
+    #print(k)
+
+#for v in articulos.values():
+    #print(v)   
+
+#for k,v in articulos.items():
+    #print(k,v)    
+
+# dicionarios anidados 
+
+familia = {
+    "hijos1" : {
+        "nombre":"Pedro",
+        "edad": 8
+
+    },
+
+    "hijos2" : {
+        "nombre":"Ana",
+         "edad": 6
+
+            
+    },
+    
+    "hijos3" : {
+        "nombre":"Marcelo",
+        "edad": 3
+
+            
+    }
+
+
+
+
+
+
+
+}
+
+
+print(familia["hijos1"]["edad"])
